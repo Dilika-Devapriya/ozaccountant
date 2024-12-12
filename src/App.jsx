dab43642ffd4d42f307518721457927eeb6002bd
Dilika-Devapriya <dilika.ramsith@gmail.com>
2024-12-12 05:56:21 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+// eslint-disable-next-line no-unused-vars
+import React from 'react'
+import Navabar from './Componets/Navbar/Navabar'
+import Hero from './Componets/Hero/Hero'
+import Shape from './Componets/Shape/Shape'
+import Section1 from './Componets/Section1.jsx/Section'
+
+const App = () => {
+  return (
+    <div>
+      <Navabar/>
+      <Hero/>
+      <Shape/>
+      <Section1/>
+    </div>
+  )
+}
+
+export default App
